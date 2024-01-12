a2d7a5c000ee3fe170576fc9db1bacb762d9f461
chanwu <125777585+chnwu-newlec@users.noreply.github.com>
2024-01-12 18:12:44 +0900

    chore: update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  ignoreDeadLinks: true, //FIXME
   base: "/newtil-css/",
   title: "Newtil CSS",
   description: "Utility based CSS",
