06ecb34a528a83c13a0f2dc016da30f8d79ae7f2
chnwu-newlec <chnwu.kim@newlecture.com>
2024-02-01 10:09:55 +0900

    chore: icon file test
    * cleanup: google_logo.svg

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,27 @@
 import postcss from "rollup-plugin-postcss";
 import postcssImport from "postcss-import";
 import url from "postcss-url";
+import copy from "rollup-plugin-copy";
+import fs from "fs";
+
+function generateIconListFromResource() {
+  return {
+    name: "generate-icons-list-from-resource",
+    buildStart() {
+      const iconListDirectory = "./packages/icon";
+      const outputPath = "./docs/data/icon-list.json";
+
+      const fileList = fs.readdirSync(iconListDirectory);
+
+      const iconNameList = fileList
+        .filter((file) => file.endsWith(".svg"))
+        .map((file) => file.replace(".svg", ""));
+
+      const jsonContent = JSON.stringify(iconNameList, null, 2);
+      fs.writeFileSync(outputPath, jsonContent, "utf8");
+    },
+  };
+}
 
 export default {
   input: "packages/css/style.css",
@@ -14,11 +35,16 @@ export default {
         postcssImport(),
         // https://github.com/postcss/postcss-url
         url({
-          url: "inline",
+          // url("../icon/name.svg") -> url("icon/name.svg")
+          url: (asset) => asset.url.replace(/\.\.\//g, ""),
         }),
       ],
       extract: true,
       minimize: true,
+    }),
+    generateIconListFromResource(),
+    copy({
+      targets: [{ src: "packages/icon/**.svg", dest: "dist/icon" }],
     }),
   ],
 };
